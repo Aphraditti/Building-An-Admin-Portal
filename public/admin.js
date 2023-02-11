5cfb2ea4df67042e4fa8d2670de210827fd87e0f
Aphra Hiat <aphrahiat@Aphras-MBP.lan>
2023-02-11 12:37:47 -0800

    Updated JavaScrip file

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,44 @@
-
 // Your Code Here
+
+let bookContainer = document.getElementById("root");
+async function displayBooks() {
+  let response = await fetch("http://localhost:3001/listBooks"); //Request sent to server and usually takes some time 
+  let books = await response.json();
+  console.log(books);
+  books.forEach((book) => {
+    bookContainer.innerHTML += `<span>${book.title}</span><input  id=${book.id} type="text" value=${book.quantity}><button type="submit">save</button><br>`;
+  });
+
+  let buttons = document.querySelectorAll("button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {              //Click function is added 
+      updateQuantity(                                            //Updating specific book
+        Number(e.target.previousSibling.id),
+        e.target.previousSibling.value
+      );
+    });
+  });
+}
+
+displayBooks();
+
+async function updateQuantity(id, quantity) {                    //Updated quantity for books
+  let response = await fetch("http://localhost:3001/updateBook", {
+    method: "PATCH",                                             //Update method
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({                                       // Convert object to JSON 
+      id: id,
+      quantity: quantity,
+    }),
+  });
+
+  let data = await response.json();
+  console.log(data); 
+}
+
+
+
+
